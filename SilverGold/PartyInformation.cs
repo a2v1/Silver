3b4192b0234fb1d38165930b60d30a7cc4066bdb
a2v1 <rakesh.patidar30@gmail.com>
2017-03-24 18:30:01 +0530

    Design Party Matser In Processing

--- SilverGold/PartyInformation.cs
+++ SilverGold/PartyInformation.cs
@@ -24,12 +24,81 @@ namespace SilverGold
             InitializeComponent();
         }
 
+        #region Helper
+
+        private void BindCreditPeriod()
+        {
+            DataGridViewColumn col1 = new DataGridViewTextBoxColumn();
+            col1.DataPropertyName = "SubGroup";
+            col1.HeaderText = "SubHead";
+            col1.Name = "SubGroup";
+            dataGridView1.Columns.Add(col1);
+        }
+
+        #endregion
+
+
+
         private void PartyInformation_Load(object sender, EventArgs e)
         {
             this.Width = CommanHelper.FormX;
             this.Height = CommanHelper.FormY;
             con = new OleDbConnection();
             con.ConnectionString = ConnectionClass.LoginConString(CommanHelper.Com_DB_PATH, CommanHelper.Com_DB_NAME + ".mdb");
+            
+            for (int i = 0; i <= 365; i++)
+            {
+                cmbdays.Items.Add(i);
+            }
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbtype_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbtype.Text.Trim() == "PARTY")
+                {
+                    cmbLot.SelectedIndex = -1;
+                    cmbLot.Enabled = false;
+                    cmbLot.Visible = false;
+                    lblLot.Visible = false;
+                }
+                else
+                {
+                    lblLot.Visible = true;
+                    cmbLot.Enabled = true;
+                    cmbLot.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
