e5a734dfec15e2e042defafcb4e76f0109c3f73e
a2v1 <rakesh.patidar30@gmail.com>
2017-12-15 17:58:03 +0530

    UserManagement Form Add

---
+++ SilverGold/Utilities/UserManagement.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace SilverGold.Utilities
+{
+    partial class UserManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnRevokePermission = new System.Windows.Forms.Button();
+            this.btnGrantPermission = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CmbCompName = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.CmbUserId = new System.Windows.Forms.ComboBox();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(415, 34);
+            this.panel2.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(15, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "User Management";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnRevokePermission);
+            this.groupBox2.Controls.Add(this.btnGrantPermission);
+            this.groupBox2.Location = new System.Drawing.Point(265, 45);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(138, 101);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Location = new System.Drawing.Point(10, 66);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnRevokePermission
+            // 
+            this.btnRevokePermission.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRevokePermission.Location = new System.Drawing.Point(10, 41);
+            this.btnRevokePermission.Name = "btnRevokePermission";
+            this.btnRevokePermission.Size = new System.Drawing.Size(120, 23);
+            this.btnRevokePermission.TabIndex = 1;
+            this.btnRevokePermission.Text = "Revoke Permission";
+            this.btnRevokePermission.UseVisualStyleBackColor = true;
+            this.btnRevokePermission.Click += new System.EventHandler(this.btnRevokePermission_Click);
+            // 
+            // btnGrantPermission
+            // 
+            this.btnGrantPermission.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGrantPermission.Location = new System.Drawing.Point(10, 17);
+            this.btnGrantPermission.Name = "btnGrantPermission";
+            this.btnGrantPermission.Size = new System.Drawing.Size(120, 23);
+            this.btnGrantPermission.TabIndex = 0;
+            this.btnGrantPermission.Text = "Grant Permission";
+            this.btnGrantPermission.UseVisualStyleBackColor = true;
+            this.btnGrantPermission.Click += new System.EventHandler(this.btnGrantPermission_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.CmbCompName);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.CmbUserId);
+            this.groupBox1.Location = new System.Drawing.Point(9, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 101);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "UserDetails";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select Company:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "UserId:";
+            // 
+            // CmbCompName
+            // 
+            this.CmbCompName.FormattingEnabled = true;
+            this.CmbCompName.Location = new System.Drawing.Point(102, 64);
+            this.CmbCompName.Name = "CmbCompName";
+            this.CmbCompName.Size = new System.Drawing.Size(133, 21);
+            this.CmbCompName.TabIndex = 2;
+            this.CmbCompName.Enter += new System.EventHandler(this.CmbCompName_Enter);
+            this.CmbCompName.Leave += new System.EventHandler(this.CmbCompName_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(102, 39);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(133, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // CmbUserId
+            // 
+            this.CmbUserId.FormattingEnabled = true;
+            this.CmbUserId.Location = new System.Drawing.Point(102, 14);
+            this.CmbUserId.Name = "CmbUserId";
+            this.CmbUserId.Size = new System.Drawing.Size(133, 21);
+            this.CmbUserId.TabIndex = 0;
+            this.CmbUserId.Enter += new System.EventHandler(this.CmbUserId_Enter);
+            this.CmbUserId.Leave += new System.EventHandler(this.CmbUserId_Leave);
+            // 
+            // UserManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 150);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UserManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "User Management";
+            this.Load += new System.EventHandler(this.UserManagement_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnRevokePermission;
+        private System.Windows.Forms.Button btnGrantPermission;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CmbCompName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox CmbUserId;
+    }
+}
